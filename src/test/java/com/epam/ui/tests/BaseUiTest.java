b01f19fce701f4f3dea7e9abfd9e8acf046a1ff4
Oleksandr Nelipa <oleksandr_nelipa@epam.com>
2023-04-23 22:25:40 +0300

    Created test for Launches Tab

--- src/test/java/com/epam/ui/tests/BaseUiTest.java
+++ src/test/java/com/epam/ui/tests/BaseUiTest.java
@@ -1,12 +1,14 @@
 package com.epam.ui.tests;
 
 import com.codeborne.selenide.junit5.ScreenShooterExtension;
+import com.epam.extensions.ui.CloseSelenideDriverExtension;
 import com.epam.extensions.ui.EnableAllureExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({
         EnableAllureExtension.class,
-        ScreenShooterExtension.class
+        ScreenShooterExtension.class,
+        CloseSelenideDriverExtension.class
 })
 public class BaseUiTest {
 }
