83a7785f6ed1fa96b7bdcaa718f6974585377731
Oleksandr Nelipa <oleksandr_nelipa@epam.com>
2023-05-02 21:31:24 +0300

    Added Allure reporting

--- src/main/java/com/epam/stepdefinitions/Hooks.java
+++ src/main/java/com/epam/stepdefinitions/Hooks.java
@@ -1,9 +1,11 @@
 package com.epam.stepdefinitions;
 
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java8.En;
 import io.cucumber.java8.Scenario;
+import io.qameta.allure.selenide.AllureSelenide;
 import lombok.extern.log4j.Log4j2;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
@@ -23,5 +25,12 @@ public class Hooks implements En {
     @BeforeAll
     public static void setupSelenide() {
         baseUrl = configuration().baseUrl();
+        SelenideLogger.addListener("AllureSelenide", allureSelenide());
+    }
+
+    private static AllureSelenide allureSelenide() {
+        return new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true);
     }
 }
